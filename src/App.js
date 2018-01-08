7a49216ea2669ce111d14da13a4c83b30a43745e
Morgan Kestner <morganleekestner@gmail.com>
2018-01-07 19:17:36 -0500

    added props and state to contacts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import  ListContacts from './ListContacts'
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:8080/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:8080/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:8080/tyler.jpg"
-  }
-]
+import * as ContactsAPI from './utils/ContactsAPI'
+
 class App extends Component {
+    state = {
+      contacts: []
+    }
+    componentDidMount() {
+      ContactsAPI.getAll().then((contacts) => {
+        this.setState({contacts})
+      })
+    }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+    ContactsAPI.remove(contact)
+  }
   render() {
     return (
       <div>
-        <ListContacts  contacts={contacts} /> 
+        <ListContacts 
+          onDeleteContact={this.removeContact} 
+          contacts={this.state.contacts}
+        /> 
       </div>
     )
   }
